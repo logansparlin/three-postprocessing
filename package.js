e342dc41df9db2f68bee0fe5be9f1e2b8135f700
Logan Sparlin <logan.sparlin@gmail.com>
2015-03-20 15:43:56 -0500

    no message

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  name: 'ls:three-postprocessing',
-  version: '0.0.1',
+  name: 'logansparlin:three-postprocessing',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Postprocessing script extensions for the 3D library three.js',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/logansparlin/three-postprocessing.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  dependencies: 'three.js'
 });
 
 Package.onUse(function(api) {
@@ -17,6 +18,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('ls:three-postprocessing');
+  api.use('logansparlin:three-postprocessing');
   api.addFiles('three-postprocessing-tests.js');
 });
